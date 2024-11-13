8c15fc8f686b7085424f65089d6f63264b1a7cc8
KitsuneX07 <144137119+KitsuneX07@users.noreply.github.com>
2024-11-13 19:12:40 +0800

    Add i18n support for zh_CN & ja_JP.

--- DownloadManager/main.py
+++ DownloadManager/main.py
@@ -1,16 +1,26 @@
 import os
 import sys
-from qfluentwidgets import setTheme, setThemeColor, Theme
+from qfluentwidgets import setTheme, setThemeColor, Theme, FluentTranslator
 from download_manager import DownloadManager
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QTranslator
 from common.config import cfg
 
 
 def main():
-    # os.chdir(os.path.join(os.path.dirname(__file__), "..", ".."))
     app = QApplication(sys.argv)
+    translator = QTranslator()
+    
     theme = cfg.get(cfg.theme)
     theme_color = cfg.get(cfg.themeColor)
+    locale = cfg.get(cfg.language).value
+    fluent_translator = FluentTranslator(locale=locale)
+
+    i18n_path = os.path.join(os.path.dirname(__file__), "resource", "i18n", f"app_{locale.name()}.qm")
+    # print(i18n_path)
+    translator.load(i18n_path)
+    app.installTranslator(translator)
+    app.installTranslator(fluent_translator)
     
     if theme == "Dark":
         setTheme(Theme.DARK)
@@ -26,4 +36,4 @@ def main():
     
     
 if __name__ == '__main__':
-    main()    
+    main()
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ComfyMSS
 
-目前仅能作为dlc在[MSST-WebUI整合包](https://github.com/SUC-DriverOld/MSST-WebUI)使用。下载整合包或git clone 对应分支最新代码以体验。
+目前仅能作为dlc在[MSST-WebUI整合包](https://github.com/SUC-DriverOld/MSST-WebUI)使用。``git clone``对应分支最新代码或参考以下方法安装。
